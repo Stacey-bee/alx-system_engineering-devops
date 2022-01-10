d768a8c79ca2c550e86283f7cf8a66ab1b0fdf51
Stacey-bee <staceynakanwagi@gmail.com>
2022-01-10 11:40:47 +0300

    creating a file using puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-# creates a file
+-- creating a file
 file {'school':
-  path    => '/tmp/school',
-  content => 'I love Puppet',
-  owner   => 'www-data',
-  group   => 'www-data',
-  mode    => '0744',
+path    => '/tmp/school',
+mode    => '0744',
+owner   => 'www-data',
+group   => 'www-data',
+content => 'I love Puppet'
 }
