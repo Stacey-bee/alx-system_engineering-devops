0bb745740ef16ac402d9ada39579554fc700f1eb
Stacey-bee <staceynakanwagi@gmail.com>
2022-01-10 13:10:56 +0300

    modifying script structure to correct check error

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # a manifest that kills a process called killmenow
-exec{'killmenow':
-command => 'pkill -n killmenow',
-path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-onlyif => 'test `pgrep killmenow|wc -l` -ge 1',
+exec { 'killmenow':
+command  => 'pkill -n killmenow',
+path     => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+onlyif   => 'test `pgrep killmenow|wc -l` -ge 1',
 }
