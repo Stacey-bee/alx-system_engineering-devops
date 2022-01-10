786e32c4e6b9d1b5c479bc3ca1fdeb278d8278e1
Stacey-bee <staceynakanwagi@gmail.com>
2022-01-10 13:07:51 +0300

    a manifest that kills a process named killmenow

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
-# kills a process named killmenow
-exec { 'killmenow':
-  command => 'pkill -n killmenow',
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  onlyif  => 'test `pgrep killmenow|wc -l` -ge 1',
+# a manifest that kills a process called killmenow
+exec{'killmenow':
+command => 'pkill -n killmenow',
+path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+onlyif => 'test `pgrep killmenow|wc -l` -ge 1',
 }
